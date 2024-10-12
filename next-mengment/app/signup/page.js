500d9f25d0df611705d97312051f6b05fd84b3af
ABDULLAH SHAKER <19490374-ABDULLAH-SHAKER@users.noreply.gitlab.com>
2024-10-12 18:34:11 +0300

    restore signup code

--- next-mengment/app/signup/page.js
+++ next-mengment/app/signup/page.js
@@ -1,7 +1,44 @@
-import next from "next";
-import  "bootstrap/dist/css/bootstrap.min.css";
+'use client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRouter } from 'next/navigation'; 
+import './page.css';
+import 'boxicons';
+export default function Home() {
+  const router = useRouter(); 
 
-export default function Page() {
-    return <h1>Hello, Home page!</h1> 
-  }
-  
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    router.push('./Home'); 
+  };
+
+  return (
+    <div className="wrapper">
+  <form onSubmit={handleSubmit}>
+    <h1>Login</h1>
+    <div className="input-box">
+      <input type="text" placeholder="username" required="" />
+      <i className="bx bxs-user" />
+      <input type="password" placeholder="password" required="" />
+      <i className="bx bxs-lock-alt" />
+    </div>
+    <div className="remember-forgot">
+      <label>
+        <input type="checkbox" />
+        remember me
+      </label>
+      <a href="#">Forgot password!</a>
+    </div>
+    <button type="submit" className="btn">
+      Login
+    </button>
+    <div className="register-link">
+      <p>
+        Don't have an account!
+        <a href="#">Register</a>
+      </p>
+    </div>
+  </form>
+</div>
+
+  );
+}
